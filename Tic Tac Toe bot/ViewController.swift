80cbc1c58de3a6a61b6c9644e39545dc46bbd242
Braeden <bcisthebomb@gmail.com>
2019-11-06 10:36:26 -0600

    Initial Commit

---
+++ Tic Tac Toe bot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tic Tac Toe bot
+//
+//  Created by Braeden Cullen on 11/6/19.
+//  Copyright © 2019 Braeden Cullen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Tic Tac Toe botTests/Tic_Tac_Toe_botTests.swift
@@ -0,0 +1,34 @@
+//
+//  Tic_Tac_Toe_botTests.swift
+//  Tic Tac Toe botTests
+//
+//  Created by Braeden Cullen on 11/6/19.
+//  Copyright © 2019 Braeden Cullen. All rights reserved.
+//
+
+import XCTest
+@testable import Tic_Tac_Toe_bot
+
+class Tic_Tac_Toe_botTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
